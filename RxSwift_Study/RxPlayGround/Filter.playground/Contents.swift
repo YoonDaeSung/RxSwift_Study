fd916e7fb7c34464d3989929ffb6b2170c1645c9
daesungYOON <anipen@Anipenui-MacBookPro.local>
2022-04-06 16:18:33 +0900

    push for commit

---
+++ RxSwift_Study/RxPlayGround/Filter.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+import RxSwift
+
+let disposeBag = DisposeBag()
+
+print("--skip--")
+Observable.of("1","2","3","4","5")
+    .skip(4)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+
+
+
+print("--skipWhile--")
+Observable.of("1","2","3","4","5","6","7","8")
+    .skip(while: {
+        $0 != "4"
+    })
+    .subscribe(onNext: {
+        print($0)
+    })
+.disposed(by: disposeBag)
+
+
+print("--skipUntil--")
+let customer = PublishSubject<String>()
+let openDoor = PublishSubject<String>()
+
+
+
+customer
+    .skip(until: openDoor)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+customer.onNext("1")
+customer.onNext("2")
+
+openDoor.onNext("open!!")
+
+customer.onNext("3")
+
+print("--take--")
+Observable<String>.of("11","22","333","444","5","6")
+    .take(3)
+    .subscribe(onNext: {
+        print($0)
+    })
+.disposed(by: disposeBag)
+
+
+print("--takeWhile--")
